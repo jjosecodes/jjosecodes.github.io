9d55dafc86e9ea74402c33c43223dbb54959c85b
jjosecodes <josejob123121@gmail.com>
2023-12-11 16:58:09 -0600

    changes

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,17 @@
+export default function HeroSection() {
+    return(
+        <section id = "hero-section" className="hero--section">
+            <div classsName="hero--section--content---box">
+                <div className="helo--section-content">
+                 <p className="section--title">Hey, I am Jose</p>
+                 <h1 className="hero--section--title">
+                    <span className="hero--section-title--color"> Full stack
+
+                    </span>
+
+                 </h1>
+                </div>
+            </div>
+        </section>
+    );
+}
