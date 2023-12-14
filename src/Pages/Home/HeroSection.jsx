59b2e23d9ddedb8587f6edf4d7bcf3e058510f90
jjosecodes <josejob123121@gmail.com>
2023-12-13 21:44:53 -0600

    frame done bar

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,16 +1,23 @@
 export default function HeroSection() {
     return(
-        <section id = "hero-section" className="hero--section">
+        <section id = "heroSection" className="hero--section">
             <div classsName="hero--section--content---box">
                 <div className="helo--section-content">
                  <p className="section--title">Hey, I am Jose</p>
                  <h1 className="hero--section--title">
-                    <span className="hero--section-title--color"> Full stack
-
-                    </span>
-
+                    <span className="hero--section-title--color"> Full stack </span>{" "}
+                     <br />
+                     Developer
                  </h1>
+                 <p className="helo--section-description" >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br /> Lorem ipsum dolor sit amet.
+                 </p>
                 </div>
+                <button className="btn btn-ptimary">Get In Touch </button>
+            </div>
+            <div className="hero--section-img">
+                <img src="./img/hero_img.png" alt="Hero Section" />
             </div>
         </section>
     );
