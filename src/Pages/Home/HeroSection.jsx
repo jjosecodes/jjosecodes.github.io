84dbbe1b502756e81b6659312f1f2d08f848b1f9
jjosecodes <111017797+jjosecodes@users.noreply.github.com>
2023-12-28 18:55:32 -0600

    should be okay

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,25 +1,35 @@
+import { Link } from 'react-scroll';
+
+
 export default function HeroSection() {
-    return (
-      <section id="heroSection" className="hero--section">
-        <div className="hero--section--content--box">
-          <div className="hero--section--content">
-            <p className="section--title">Greetings! I am Jose. Feel free to explore some of my ongoing and completed works right here!</p>
-            <h1 className="hero--section--title">
-              <span className="hero--section-title--color">Full Stack</span>{" "}
-              <br />
-              Developer
-            </h1>
-            <p className="hero--section-description">
-              
-              <br /> 
-            </p>
-          </div>
-          <button className="btn btn-primary">Get In Touch</button>
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Greetings! I am Jose. Feel free to explore some of my ongoing and completed works right here!</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            <br /> 
+          </p>
         </div>
-        <div className="hero--section--img">
-          <img src="./img/hero_img.png" alt="Hero Section" />
-        </div>
-      </section>
-    );
-  }
-  
+        <Link
+          to="Contact"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className="btn btn-primary"
+        >
+          Get In Touch
+        </Link>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
